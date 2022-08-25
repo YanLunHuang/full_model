495f8427a4a6068c5af419cb51d1110dfbf53433
YanLun-Huang <94730012+YanLunHuang@users.noreply.github.com>
2022-08-25 18:13:08 +0800

    Update nnet_conv2d_stream.h

--- firmware/nnet_utils/nnet_conv2d_stream.h
+++ firmware/nnet_utils/nnet_conv2d_stream.h
@@ -211,7 +211,12 @@ void conv_2d_large_cl_nopad_pad_me(
     #pragma HLS ARRAY_RESHAPE variable=tmpdata complete
 
     static data_T layer_in[CONFIG_T::filt_height*CONFIG_T::filt_width*CONFIG_T::n_chan];
-    #pragma HLS ARRAY_RESHAPE variable=layer_in complete
+    if(data_T::width*CONFIG_T::filt_height*CONFIG_T::filt_width*CONFIG_T::n_chan >= 65536){
+	#pragma HLS ARRAY_RESHAPE variable=layer_in block factor=CONFIG_T::n_chan
+    }
+    else {
+	#pragma HLS ARRAY_RESHAPE variable=layer_in complete
+    }
 
     //typename res_T::value_type layer_reluout[CONFIG_T::n_filt];
     //#pragma HLS ARRAY_RESHAPE variable=layer_reluout complete dim=0
